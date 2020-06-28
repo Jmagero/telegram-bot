d16b61c4d61a97538e3b033a3664dddf6f0dd42f
jmagero <jmagero2017@gmail.com>
2020-06-28 19:52:06 +0300

    Add make_request method

--- lib/quote.rb
+++ lib/quote.rb
@@ -10,4 +10,11 @@ class Quote
     @results = make_request
   end
 
+  def make_request
+    url = 'https://programming-quotes-api.herokuapp.com/quotes'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    response = JSON.parse(response)
+    response
+  end
 end
