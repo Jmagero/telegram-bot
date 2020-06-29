d7caf28df0ba4beb503baca16501a95da0ad5355
jmagero <jmagero2017@gmail.com>
2020-06-29 19:00:00 +0300

    Fix rubocop errors

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Layout/LineLength
 require 'telegram/bot'
 require_relative 'quote.rb'
 
@@ -9,18 +10,17 @@ class Bot
     bot.listen do |message|
       case message.text
       when '/start'
-        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name},Welcome to programming inspiration chat bot created by Jocyline. The bot is to inspire you while you are coding. Use /start to start the chat bot, /stop to end the chat bot and /quote to get an inspiration quote")
+          bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name},Welcome to inspiration chat.Use /start to start the chat bot, /stop to end the chat bot and /quote to get an inspiration quote")
       when '/stop'
         bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
       when '/quote'
         result = Quote.new
         new_result = result.select_random
-        bot.api.send_message(chat_id: message.chat.id, text: "#{new_result['en']}", date: message.date)
+          quote = new_result['en']
+          bot.api.send_message(chat_id: message.chat.id, text: quote.to_s, date: message.date)
       end
     end
   end
 end
 end
-
-
-
+# rubocop:enable Layout/LineLength
