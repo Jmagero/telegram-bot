24aad75ff0659cf635e5cc16bf4062cd4b298cc9
jmagero <jmagero2017@gmail.com>
2020-06-29 19:50:59 +0300

    Fix rubocop offenses

--- Gemfile
+++ Gemfile
@@ -1,5 +1,3 @@
 gem 'json'
 gem 'net-http-persistent', '~> 2.9', '>= 2.9.4'
 gem 'telegram-bot-ruby'
-gem  'json'
-gem 'net-http-persistent', '~> 2.9', '>= 2.9.4'
--- spec/telegram_bot_tests.rb
+++ spec/telegram_bot_tests.rb
@@ -2,8 +2,8 @@ require_relative '..lib/quote.rb'
 
 Rspec.describe Quote do
   describe 'make_request' do
-    let(:request) {Quote.new}
-    let(:request_json) {request.make_request}
+    let(:request) { Quote.new }
+    let(:request_json) { request.make_request }
 
     it 'Should return json response when the request is successful' do
       expect(request_json.class).to eql(Array)
@@ -11,11 +11,11 @@ Rspec.describe Quote do
   end
 
   describe '#select_random' do
-    let(:random) {Quote.new}
-    let(:request_random) {random.select_random}
+    let(:random) { Quote.new }
+    let(:request_random) { random.select_random }
 
     it 'Should return an Hash' do
-      expect(request_random.class).to eql (Hash)
+      expect(request_random.class).to eql Hash
     end
 
     it 'Should return a key and value' do
