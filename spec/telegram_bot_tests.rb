ec38c02c7718fb51260de7163596ac2c90d33657
jmagero <jmagero2017@gmail.com>
2020-06-29 19:02:37 +0300

    Add tests for select_random method

--- spec/telegram_bot_tests.rb
+++ spec/telegram_bot_tests.rb
@@ -8,4 +8,16 @@ Rspec.describe Quote do
     end
   end
 
+  describe '#select_random' do
+    let(:random) {Quote.new}
+    let(:request_random) {random.select_random}
+
+    it 'Should return an Hash' do
+      expect(request_random.class).to eql (Hash)
+    end
+
+    it 'Should return a key and value' do
+      expect(request_random.length).to eql(2)
+    end
+  end
 end
