ec76077738a53ff32b5fb929de4a2f9014afa808
jmagero <jmagero2017@gmail.com>
2020-06-29 19:01:12 +0300

    Add rspec

--- lib/bot.rb
+++ lib/bot.rb
@@ -5,22 +5,22 @@ require_relative 'quote.rb'
 class Bot
   def initialize
     token = '833112055:AAGQxUtUr1zso5tHk3vKJcVCex3ViMLtNm0'
-  
-  Telegram::Bot::Client.run(token) do |bot|
-    bot.listen do |message|
-      case message.text
-      when '/start'
+
+    Telegram::Bot::Client.run(token) do |bot|
+      bot.listen do |message|
+        case message.text
+        when '/start'
           bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name},Welcome to inspiration chat.Use /start to start the chat bot, /stop to end the chat bot and /quote to get an inspiration quote")
-      when '/stop'
-        bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
-      when '/quote'
-        result = Quote.new
-        new_result = result.select_random
+        when '/stop'
+          bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+        when '/quote'
+          result = Quote.new
+          new_result = result.select_random
           quote = new_result['en']
           bot.api.send_message(chat_id: message.chat.id, text: quote.to_s, date: message.date)
+        end
       end
     end
   end
-end
 end
 # rubocop:enable Layout/LineLength
