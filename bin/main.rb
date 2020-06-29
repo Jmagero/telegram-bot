bc07acc20a2ceebf06384a9e6bd7862a887c56ef
jmagero <jmagero2017@gmail.com>
2020-06-29 20:45:50 +0300

    Add 'inpiration'to the puts

--- bin/main.rb
+++ bin/main.rb
@@ -3,7 +3,7 @@ require_relative '../lib/quote.rb'
 
 puts 'Helo!, Welcome to inspirational telegram bot'
 puts '--------------------------------------------'
-puts 'This is a bot to help you stay inspired while coding by giving you inspiration programming quotes'
+puts 'This is a bot to help you stay inspired while coding by giving you inspiration quotes'
 puts '--------------------------------------------'
 puts '....loading the bot is now online... use ctrl-c to stop the bot'
 
