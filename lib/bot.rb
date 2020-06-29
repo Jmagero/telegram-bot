319e2edc9f4d0ab165b40029d0a861f64c145ea4
jmagero <jmagero2017@gmail.com>
2020-06-29 20:44:54 +0300

    Add source & quote value

--- lib/bot.rb
+++ lib/bot.rb
@@ -10,13 +10,15 @@ class Bot
       bot.listen do |message|
         case message.text
         when '/start'
-          bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name},Welcome to inspiration chat.Use /start to start the chat bot, /stop to end the chat bot and /quote to get an inspiration quote")
+          bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name},Welcome to inspiration chat bot.Use /start to start the chat bot, /stop to end the chat bot and /quote to get an inspiration quote")
         when '/stop'
           bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
         when '/quote'
           result = Quote.new
           new_result = result.select_random
-          quote = new_result['en']
+          quote = new_result['quote']
+          source = new_result['source']
+          bot.api.send_message(chat_id: message.chat.id, text: source.to_s, date: message.date)
           bot.api.send_message(chat_id: message.chat.id, text: quote.to_s, date: message.date)
         end
       end
