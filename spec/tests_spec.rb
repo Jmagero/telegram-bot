6a63dc749d6bd8e72a537e2da69a138799dd18a2
jmagero <jmagero2017@gmail.com>
2020-06-30 17:49:42 +0300

    Remove tests for make_request private method

--- spec/tests_spec.rb
+++ spec/tests_spec.rb
@@ -1,15 +1,6 @@
 require_relative '../lib/quote.rb'
 
 describe Quote do
-  describe 'make_request' do
-    let(:request) { Quote.new }
-    let(:request_json) { request.make_request }
-
-    it 'Should return json response when the request is successful' do
-      expect(request_json.class).to eql(Array)
-    end
-  end
-
   describe '#select_random' do
     let(:random) { Quote.new }
     let(:request_random) { random.select_random }
