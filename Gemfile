e93f5b8e5561962fa55f788eabb4b96ad68a5a03
jmagero <jmagero2017@gmail.com>
2020-07-01 18:14:13 +0300

    Fix rubocop errors

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'https://rubygems.org'
 
 gem 'json'
 gem 'net-http-persistent', '~> 2.9', '>= 2.9.4'
+gem 'rspec'
 gem 'telegram-bot-ruby'
--- spec/quote_spec.rb
+++ spec/quote_spec.rb
@@ -4,11 +4,9 @@ describe Quote do
   describe '#select_random' do
     let(:random) { Quote.new }
     let(:request_random) { random.select_random }
-
     it 'Should return an Hash' do
       expect(request_random.class).to eql Hash
     end
-
     it 'Should return a key and value' do
       expect(request_random.length).to eql(4)
     end
